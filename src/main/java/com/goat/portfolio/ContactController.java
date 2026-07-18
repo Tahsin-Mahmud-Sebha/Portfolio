59394f4d58a0a694e45cc5b88e3e4f24a1f22a86
Tahsin-Mahmud-Sebha <Sebhatahsin22@gmail.com>
2026-07-18 23:47:11 +0600

    Version updated

--- src/main/java/com/goat/portfolio/ContactController.java
+++ src/main/java/com/goat/portfolio/ContactController.java
@@ -1,8 +1,6 @@
 package com.goat.portfolio;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
@@ -14,9 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class ContactController {
 
-    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
-
-    // We only need the MailSender now!
     private final JavaMailSender mailSender;
 
     @GetMapping("/")
@@ -26,23 +21,21 @@ public class ContactController {
 
     @PostMapping("/submit-form")
     public String submitForm(@RequestParam String name, @RequestParam String email, @RequestParam String message) {
-        log.info("Processing form submission from: {}", name);
-
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setTo("sebhatahsin22@gmail.com"); // Sending it to yourself
-            mailMessage.setReplyTo(email); // So you can hit "Reply" and email them back
+            mailMessage.setTo("sebhatahsin22@gmail.com");
+            mailMessage.setReplyTo(email);
             mailMessage.setSubject("New Portfolio Message from: " + name);
             mailMessage.setText("Name: " + name + "\nEmail: " + email + "\n\nMessage:\n" + message);
 
             mailSender.send(mailMessage);
-            log.info("Email successfully sent!");
 
         } catch (Exception e) {
-            log.error("Failed to send email: ", e);
+            // We removed the log, so this just prints the error to the server console if it fails
+            e.printStackTrace();
         }
 
-        // Redirect back to the page so they don't get stuck on a blank screen
+        // Redirect back to the main page
         return "redirect:/";
     }
 }
